021ccdcd44c0fff990d2c82cee1e975ae08d7e53
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-01-12 12:57:48 +0800

    Update xray_cert_renew.sh

--- xray/xray_cert_renew.sh
+++ xray/xray_cert_renew.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
+/root/.acme.sh/acme.sh --issue -d "www.cinima.asia"  --server letsencrypt  --keylength ec-256  --fullchain-file /usr/local/etc/xray/cert.pem  --key-file /usr/local/etc/xray/key.pem  --standalone  --force
 echo "Xray Certificates Renewed"
 
-chmod +r /usr/local/etc/xray/xray.key
+chmod +r /usr/local/etc/xray/key.pem
 echo "Read Permission Granted for Private Key"
 
 sudo systemctl restart xray
