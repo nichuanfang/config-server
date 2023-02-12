be1a3b5bafbaf1b8c4220c550f1c4b95c42480d6
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-02-12 23:08:53 +0800

    Update build.sh

--- ngrok/build.sh
+++ ngrok/build.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
 # ubuntu系统
-apt-get install build-essential golang mercurial git
-cd /usr/local
-git clone https://github.com/tutumcloud/ngrok.git ngrok
-NGROK_DOMAIN="$0"
-cd ngrok
-openssl genrsa -out base.key 2048
-openssl req -new -x509 -nodes -key base.key -days 10000 -subj "/CN=$NGROK_DOMAIN" -out base.pem
-openssl genrsa -out server.key 2048
-openssl req -new -key server.key -subj "/CN=$NGROK_DOMAIN" -out server.csr
-openssl x509 -req -in server.csr -CA base.pem -CAkey base.key -CAcreateserial -days 10000 -out server.crt
+apt-get update
+apt-get install git
+rm -rf /usr/local/ngrok
+cd /usr/local/
+git clone https://github.com/tutumcloud/ngrok.git
+export NGROK_DOMAIN="$0" #你自己的二级域名
+cd /usr/local/ngrok/ #这里是你自己的ngrok路径（之前clone下载的）
+openssl genrsa -out rootCA.key 2048
+openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=$NGROK_DOMAIN" -days 5000 -out rootCA.pem
+openssl genrsa -out device.key 2048
+openssl req -new -key device.key -subj "/CN=$NGROK_DOMAIN" -out device.csr
+openssl x509 -req -in device.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out device.crt -days 5000
+cp rootCA.pem assets/client/tls/ngrokroot.crt
+cp device.crt assets/server/tls/snakeoil.crt
+cp device.key assets/server/tls/snakeoil.key
 
-cp base.pem assets/client/tls/ngrokroot.crt
+make release-server
+#下面这些是生成的客户端，可根据自己的客户端进行挑选，不用全部都导出
+#GOOS=linux GOARCH=386 make release-client 
+GOOS=linux GOARCH=amd64 make release-client
+#GOOS=windows GOARCH=386 make release-client
+GOOS=windows GOARCH=amd64 make release-client
+#GOOS=darwin GOARCH=386 make release-client
+#GOOS=darwin GOARCH=amd64 make release-client
+#GOOS=linux GOARCH=arm make release-client
