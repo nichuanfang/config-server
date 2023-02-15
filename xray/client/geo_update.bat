31e2aed19c5bcfbc7951f7348eff920f297d34fa
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-02-15 16:20:21 +0800

    windows定时任务脚本

---
+++ xray/client/geo_update.bat
@@ -0,0 +1,3 @@
+powershell curl  -o  "geoip.dat"  "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat";
+
+powershell curl  -o  "geosite.dat"  "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat";
