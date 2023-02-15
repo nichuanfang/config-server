4d4dcd4e2ea56ded30ac09d470f09eae2ef05d2f
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-02-15 16:25:18 +0800

    Update geo_update.bat

--- xray/client/geo_update.bat
+++ xray/client/geo_update.bat
@@ -1,3 +1,3 @@
-powershell curl  -o  "geoip.dat"  "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat";
+powershell curl  -o  "D:\soft\Xray-windows-64\geoip.dat"  "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat";
 
-powershell curl  -o  "geosite.dat"  "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat";
+powershell curl  -o  "D:\soft\Xray-windows-64\geosite.dat"  "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat";
