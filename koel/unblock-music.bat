d6da3b7d5d946b0d7c57f64545482e395ccacaec
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-02-22 15:00:59 +0800

    Create unblock-music.bat

---
+++ koel/unblock-music.bat
@@ -0,0 +1,3 @@
+@echo off
+E:\music_download\um.exe -o E:\music_download\ -i E:\music_download\QQ\VipSongsDownload&E:\music_download\um.exe -o E:\music_download\ -i E:\music_download\Netease\VipSongsDownload
+pause
