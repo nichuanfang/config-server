fc97d5db9c43a384c23d22e2cee13c0bc13182e6
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-02-25 13:04:01 +0800

    Update unblock-music.bat

--- koel/unblock-music.bat
+++ koel/unblock-music.bat
@@ -1,3 +1,3 @@
 @echo off
-E:\music_download\tools\um.exe -o E:\music_download\QQ -i E:\music_download\QQ\VipSongsDownload&E:\music_download\um.exe -o E:\music_download\Netease -i E:\music_download\Netease\VipSongsDownload
+E:\music_download\tools\um.exe -o E:\music_download\qq -i E:\music_download\qq\VipSongsDownload&E:\music_download\tools\um.exe -o E:\music_download\netease -i E:\music_download\netease\VipSongsDownload&del /a /f /s /q  "E:\music_download\qq\VipSongsDownload\*"& del /a /f /s /q  "E:\music_download\netease\VipSongsDownload\*"
 pause
