21daa1097616dc9b7be1cff99e5d0b7e7ca4af39
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-03-10 11:06:00 +0800

    Update recover.sh

--- mysql/backup/recover.sh
+++ mysql/backup/recover.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-# 恢复数据库
+# 从阿里云拉取数据
+cp -r -n /cloudnas/data/CloudDrive/ali-open/backup/mysql/music_info.sql /root/mysql_backup/
+# 恢复
 docker exec -i mysql sh -c 'exec mysql -uroot -p'${1}  < /root/mysql_backup/music_info.sql
