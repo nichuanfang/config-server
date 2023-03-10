6deec72cd33d590b0902b96a3aa70de487b0963a
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-03-10 09:57:43 +0800

    Create start.sh

---
+++ tinymediamanager/start.sh
@@ -0,0 +1,17 @@
+docker run \
+    --name=tinymediamanager \
+    --restart=always \
+    --network=mynet \
+    -it \
+    -d  \
+    -p 4000:4000 \
+    -p 7878:7878 \
+    -e USER_ID=0 \
+    -e GROUP_ID=0   \
+    -v /root/tmm/data:/data       \
+    -v /cloudnas/data/CloudDrive/ali-open/tmm/tmm-movies:/media/ali-movies \
+    -v /cloudnas/data/CloudDrive/ali-open/tmm/tmm-tvshows:/media/ali-tvshows \
+    -v /cloudnas/data/CloudDrive/PikPak/media/tmm/tmm-movies:/media/pikpak-movies \
+    -v /cloudnas/data/CloudDrive/PikPak/media/tmm/tmm-tvshows:/media/pikpak-tvshows \
+    -v /root/tmm/addons:/app/addons \
+    tinymediamanager/tinymediamanager:latest
