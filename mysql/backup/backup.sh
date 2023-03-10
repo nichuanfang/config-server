619f979e640f01df7d9ab7c7743bbd77fab856cf
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-03-10 11:52:34 +0800

    Update backup.sh

--- mysql/backup/backup.sh
+++ mysql/backup/backup.sh
@@ -8,4 +8,6 @@ docker exec mysql sh -c 'exec mysqldump --databases koel -u root -p'${1} >/root/
 #个人博客数据
 docker exec mysql sh -c 'exec mysqldump --databases jpress -u root -p'${1} >/root/mysql_backup/jpress.sql
 # 保存到阿里云
-cp -r /root/mysql_backup/*.sql /cloudnas/data/CloudDrive/ali-open/backup/mysql/
+#cp -r /root/mysql_backup/music_info.sql /cloudnas/data/CloudDrive/ali-open/backup/mysql/
+#cp -r /root/mysql_backup/koel.sql /cloudnas/data/CloudDrive/ali-open/backup/mysql/
+#cp -r /root/mysql_backup/jpress.sql /cloudnas/data/CloudDrive/ali-open/backup/mysql/
