50241a477bbfe948638270fa7abd822a5e1f028d
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-03-10 14:09:18 +0800

    Update start.sh

--- tinymediamanager/start.sh
+++ tinymediamanager/start.sh
@@ -9,9 +9,9 @@ docker run \
     -e USER_ID=0 \
     -e GROUP_ID=0   \
     -v /root/tmm/data:/data       \
-    -v /cloudnas/data/CloudDrive/ali-open/tmm/tmm-movies:/media/ali-movies \
-    -v /cloudnas/data/CloudDrive/ali-open/tmm/tmm-tvshows:/media/ali-tvshows \
-    -v /cloudnas/data/CloudDrive/PikPak/media/tmm/tmm-movies:/media/pikpak-movies \
-    -v /cloudnas/data/CloudDrive/PikPak/media/tmm/tmm-tvshows:/media/pikpak-tvshows \
+    -v /cloudnas/CloudDrive/ali-open/tmm/tmm-movies:/media/ali-movies \
+    -v /cloudnas/CloudDrive/ali-open/tmm/tmm-tvshows:/media/ali-tvshows \
+    -v /cloudnas/CloudDrive/PikPak/media/tmm/tmm-movies:/media/pikpak-movies \
+    -v /cloudnas/CloudDrive/PikPak/media/tmm/tmm-tvshows:/media/pikpak-tvshows \
     -v /root/tmm/addons:/app/addons \
     tinymediamanager/tinymediamanager:latest
