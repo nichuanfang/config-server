e71e2301ecead6e3e500af7ffb523e83d984ec43
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-03-10 11:13:23 +0800

    Update backup.sh

--- mysql/backup/backup.sh
+++ mysql/backup/backup.sh
@@ -2,4 +2,4 @@
 # 备份指定数据库
 docker exec mysql sh -c 'exec mysqldump --databases music_info -u root -p'${1} >/root/mysql_backup/music_info.sql
 # 保存到阿里云
-cp -r -n /root/mysql_backup/music_info.sql /cloudnas/data/CloudDrive/ali-open/backup/mysql/
+cp -r /root/mysql_backup/music_info.sql /cloudnas/data/CloudDrive/ali-open/backup/mysql/
