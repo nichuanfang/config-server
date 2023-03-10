a430540bd74afada0466e5c8e6876e47807fda50
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-03-10 11:53:13 +0800

    Update recover.sh

--- mysql/backup/recover.sh
+++ mysql/backup/recover.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-cp -r /cloudnas/data/CloudDrive/ali-open/backup/mysql/*.sql /root/mysql_backup/
-docker exec -i mysql sh -c 'exec mysql -uroot -p'${1}  < /root/mysql_backup/*.sql
+#cp -r /cloudnas/data/CloudDrive/ali-open/backup/mysql/music_info.sql /root/mysql_backup/
+#cp -r /cloudnas/data/CloudDrive/ali-open/backup/mysql/koel.sql /root/mysql_backup/
+#cp -r /cloudnas/data/CloudDrive/ali-open/backup/mysql/jpress.sql /root/mysql_backup/
+
+docker exec -i mysql sh -c 'exec mysql -uroot -p'${1}  < /root/mysql_backup/music_info.sql
+docker exec -i mysql sh -c 'exec mysql -uroot -p'${1}  < /root/mysql_backup/koel.sql
+docker exec -i mysql sh -c 'exec mysql -uroot -p'${1}  < /root/mysql_backup/jpress.sql
