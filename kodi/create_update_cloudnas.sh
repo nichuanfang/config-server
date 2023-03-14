109ccf28f11d8bbdce0d98966456d5e519792365
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-03-14 14:52:11 +0800

    Update create_update_cloudnas.sh

--- kodi/create_update_cloudnas.sh
+++ kodi/create_update_cloudnas.sh
@@ -27,7 +27,7 @@ docker rmi -f cloudnas/clouddrive2-unstable:latest
 
 # 删除原挂载目录
 if [ -d  "/storage/cloudnas/CloudDrive" ]; then
-rm -rf /storage/cloudnas/*
+rm -rf /storage/cloudnas/CloudDrive
 fi
 
 #拉取代理镜像并重命名
