93b72f904b4c2cbc4611d0e57cb324ce78444cf9
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-03-14 14:30:20 +0800

    Update create_update_cloudnas.sh

--- kodi/create_update_cloudnas.sh
+++ kodi/create_update_cloudnas.sh
@@ -6,60 +6,6 @@ echo "开始更新clouddrive2...."
 #enable shared mount option for mapped volume in host
 mount --make-shared /storage
 
-#创建相应目录赋予权限
-if [ ! -d "/storage/cloudnas"]; then
-mkdir -p /storage/cloudnas
-chmod 777 /storage/cloudnas
-fi
-
-if [ ! -d "/storage/clouddrive2/config"]; then
-mkdir -p /storage/clouddrive2/config
-chmod 777 /storage/clouddrive2/config
-fi
-
-#更新clouddrive2
-docker rm -f clouddrive2
-
-docker rmi -f cloudnas/clouddrive2-unstable:latest
-
-#拉取代理镜像并重命名
-docker pull dockerproxy.com/cloudnas/clouddrive2-unstable:latest
-docker tag dockerproxy.com/cloudnas/clouddrive2-unstable:latest cloudnas/clouddrive2-unstable:latest
-docker rmi dockerproxy.com/cloudnas/clouddrive2-unstable:latest
-
-echo "Clouddrive2更新完毕!"
-#启动容器
-echo "开始启动clouddrive2...."
-docker run -d \
-      --name clouddrive2 \
-      --restart always \
-      --env CLOUDDRIVE_HOME=/Config \
-      --env UID=0 \
-      --env GID=0 \
-      -v /storage/cloudnas:/CloudNAS:shared \
-      -v /storage/clouddrive2/config:/Config \
-      --network host \
-      --pid host \
-     --privileged \
-     --device /dev/fuse:/dev/fuse \
-     cloudnas/clouddrive2-unstable
-
-echo "clouddrive2启动完成!"
-
-exit 0
-
-
--------------------------------------------------------
-
-
-#!/bin/bash
-# Clouddrive2 install script for common linux server
-
-echo "开始更新clouddrive2...."
-
-#enable shared mount option for mapped volume in host
-mount --make-shared /
-
 #创建相应目录赋予权限     [   ]  内  前后都要有空格
 if [ ! -d "/storage/cloudnas" ]; then
 mkdir -p /storage/cloudnas
