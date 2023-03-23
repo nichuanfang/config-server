aefa62b3a3c47e80aeefc2c9667a900fc4d7af28
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-03-23 14:51:59 +0800

    Update switch-vscode-bg.sh

--- vscode/switch-vscode-bg.sh
+++ vscode/switch-vscode-bg.sh
@@ -1,7 +1,6 @@
-# !/bin/bash
-# 8以内的随机数
-img_prefix = $RANDOM % 8 + 1
-img_path = '/root/assets/img/bg'
-
+#!/bin/bash
 # 每天定时更换vscode背景图
-cp img_path/bg'$img_prefix'.jpg img_path/bg-vscode.jpg
+x=$((RANDOM % 8 + 1))
+img_path="/root/assets/img/bg/bg${x}.jpg"
+dest_path="/root/assets/img/bg/bg-vscode.jpg"
+cp $img_path $dest_path
