cdd509bc9fe566b921f7d175f8a323412acdab01
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-03-23 20:14:05 +0800

    Update renew.sh

--- nginx/renew.sh
+++ nginx/renew.sh
@@ -31,6 +31,14 @@ docker stop nginx
   --key-file /opt/docker/nginx/cert/css-vencenter-cn-key.pem \
   --standalone \
   --force
+/root/.acme.sh/acme.sh --issue \
+  -d "m3u.vencenter.cn" \
+  --server letsencrypt \
+  --keylength ec-256 \
+  --fullchain-file /opt/docker/nginx/cert/m3u-vencenter-cn.pem \
+  --key-file /opt/docker/nginx/cert/m3u-vencenter-cn-key.pem \
+  --standalone \
+  --force
 
 echo "Certificates Renewed"
 chmod 600 /opt/docker/nginx/cert/*.pem
