166c2a5ef3c648eed6a588548d0762418400fa57
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-03-23 15:10:40 +0800

    vscode每天从15张图片中随机一张作为背景图片

--- vscode/switch-vscode-bg.sh
+++ vscode/switch-vscode-bg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 每天定时更换vscode背景图
-x=$((RANDOM % 8 + 1))
+x=$((RANDOM % 15 + 1))
 img_path="/root/assets/img/bg/bg${x}.jpg"
 dest_path="/root/assets/img/bg/bg-vscode.jpg"
 cp $img_path $dest_path
