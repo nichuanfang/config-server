4a7cc23098b09008c8defc115af0e5e2c73ec4a6
chuanfang <f18326186224@gmail.com>
2023-03-24 10:48:23 +0800

    json with comments

--- tinymediamanager/renew-movies.sh
+++ tinymediamanager/renew-movies.sh
@@ -2,7 +2,7 @@
 cd /cloudnas/CloudDrive/ali-open/tmm/tmm-movies
 # 刮削电影
 curl \
--d '[{"action":"update", "scope":{"name":"all"}},{"action":"scrape", "scope":{"name":"new"}}]' \
--H "Content-Type: application/json"  \
--H "api-key: $0"  \
--X POST http://localhost:7878/api/movies
+    -d '[{"action":"update", "scope":{"name":"all"}},{"action":"scrape", "scope":{"name":"new"}}]' \
+    -H "Content-Type: application/json" \
+    -H "api-key: $0" \
+    -X POST http://localhost:7878/api/movies
