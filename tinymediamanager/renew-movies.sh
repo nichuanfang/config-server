615f754e5cbf15787855945fadd8f6fef1fb1b1d
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-03-25 11:34:53 +0800

    Update renew-movies.sh

--- tinymediamanager/renew-movies.sh
+++ tinymediamanager/renew-movies.sh
@@ -2,7 +2,7 @@
 docker restart clouddrive2 
 sleep 3s
 docker start tinymediamanager
-sleep 10s
+sleep 20s
 # 刮削电影
 curl \
     -d '[{"action":"update", "scope":{"name":"all"}},{"action":"scrape", "scope":{"name":"new"}}]' \
