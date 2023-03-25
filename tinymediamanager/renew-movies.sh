1913d204ec60a093cab18724b3a0c6e15ecde5b4
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-03-25 10:40:33 +0800

    Update renew-movies.sh

--- tinymediamanager/renew-movies.sh
+++ tinymediamanager/renew-movies.sh
@@ -8,5 +8,5 @@ curl \
     -X POST http://localhost:7878/api/movies
 sleep 1h
 cd /cloudnas/CloudDrive/ali-open/tmm/tmm-movies
-mv /cloudnas/CloudDrive/ali-open/tmm/tmm-movies/* /cloudnas/CloudDrive/ali-open/movies/
+mv ./* /cloudnas/CloudDrive/ali-open/movies/
 docker stop tinymediamanager
