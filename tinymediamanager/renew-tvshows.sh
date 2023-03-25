a2d43ab581903271e9df9dda683ce26d830032d1
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-03-25 11:31:01 +0800

    Update renew-tvshows.sh

--- tinymediamanager/renew-tvshows.sh
+++ tinymediamanager/renew-tvshows.sh
@@ -1,7 +1,8 @@
 # !/bin/bash
-docker restart clouddrive2
+docker restart clouddrive2 
+sleep 3s
 docker start tinymediamanager
-sleep 10s
+sleep 3s
 # 刮削剧集
 curl \
 -d '[{"action":"update", "scope":{"name":"all"}},{"action":"scrape", "scope":{"name":"new"}}]' \
