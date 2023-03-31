a287fc981e5547b480380dbcddc61819ac7f53d0
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-03-31 18:57:01 +0800

    Create hook.sh

---
+++ webhook/hooks/hook.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 入口
+# 公共的hook
+
+# 如果没有该项目 git clone到/root/code中
+# 有该项目 执行git pull 更新文件
+# 执行build.sh
+
+if [ ! -d "/root/code/$1" ]; then
+    cd /root/code
+    git clone $2
+else
+    git pull
+fi
+cd /root/code/$1
+# 检出指定分支
+git checkout ${3:11}
+# 执行指定脚本
+bash build.sh
