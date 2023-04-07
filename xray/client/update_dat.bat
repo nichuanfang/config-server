85528377f6ff208f4545706baf38dc0daf75477a
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-04-07 18:32:43 +0800

    Create update_dat.bat

---
+++ xray/client/update_dat.bat
@@ -0,0 +1,2 @@
+powershell curl  -o  "D:\soft\Xray-windows-64\geoip.dat"  "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat";
+powershell curl  -o  "D:\soft\Xray-windows-64\geosite.dat"  "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat";
