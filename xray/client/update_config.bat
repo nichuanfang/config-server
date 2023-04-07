f3e1803092685bf7118c261430b4d282e9886192
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-04-07 18:28:35 +0800

    Update update_config.bat

--- xray/client/update_config.bat
+++ xray/client/update_config.bat
@@ -1,2 +1,2 @@
-Invoke-WebRequest -Uri "https://config.vencenter.cn/client/config.json" -Headers @{"Authorization" = "Basic "+[System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("18326186224:0820nCf9270" ))} -Method Get -OutFile 'D:\soft\Xray-windows-64\config.json'
+Invoke-WebRequest -Uri "https://config.vencenter.cn/client/config.json" -Headers @{"Authorization" = "Basic "+[System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("用户名:密码" ))} -Method Get -OutFile 'D:\soft\Xray-windows-64\config.json'
 & .\restart.bat
