00e0f03e7b8822ea5ac0ff6ca7afb00915a2c73e
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-04-07 18:04:40 +0800

    Delete update_config.bat

--- xray/client/update_config.bat
+++
@@ -1,8 +0,0 @@
-@echo on
-powershell curl  -o  "D:\soft\Xray-windows-64\config.json"  "客户端配置文件地址";
-echo "wxray Restart"
-taskkill /f /t /fi "imagename eq wxray.exe"
-echo "wxray Stop"
-start /d "D:\soft\Xray-windows-64" wxray.exe
-echo "wxray Start"
-exit
