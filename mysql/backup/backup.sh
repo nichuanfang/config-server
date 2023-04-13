f0e2ed70f361cfb2e931bcf9a2533efdd52e8994
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-04-13 15:25:41 +0800

    Update backup.sh

--- mysql/backup/backup.sh
+++ mysql/backup/backup.sh
@@ -2,13 +2,13 @@
 # 备份指定数据库
 
 #音乐信息表
-docker exec mysql sh -c 'exec mysqldump --databases music_info -u root -p'${1} >/root/mysql_backup/music_info.sql
+docker exec mysql sh -c 'exec mysqldump --databases music_info -u root -p'${1} >/root/mysql/backup/music_info.sql
 #koel表
-docker exec mysql sh -c 'exec mysqldump --databases koel -u root -p'${1} >/root/mysql_backup/koel.sql
+docker exec mysql sh -c 'exec mysqldump --databases koel -u root -p'${1} >/root/mysql/backup/koel.sql
 #个人博客数据
-docker exec mysql sh -c 'exec mysqldump --databases jpress -u root -p'${1} >/root/mysql_backup/jpress.sql
+docker exec mysql sh -c 'exec mysqldump --databases jpress -u root -p'${1} >/root/mysql/backup/jpress.sql
 # 保存到阿里云
-cd /root/mysql_backup
+cd /root/mysql/backup
 find . -name "*.sql" | xargs tar czvf www.vencenter.cn.tar.gz
 rm -f /cloudnas/CloudDrive/ali-open/backup/mysql/www.vencenter.cn.tar.gz
 cd /cloudnas/CloudDrive/ali-open/backup/mysql/
