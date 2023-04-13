f8d2f4fb23ae293238d41caa34bc1b7f3432bd76
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-04-13 15:29:38 +0800

    Update recover.sh

--- mysql/backup/recover.sh
+++ mysql/backup/recover.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /cloudnas/CloudDrive/ali-open/backup/mysql/
-cp www.vencenter.cn.tar.gz /root/mysql_backup/
-cd /root/mysql_backup/
+cp www.vencenter.cn.tar.gz /root/mysql/backup/
+cd /root/mysql/backup/
 tar -zxvf www.vencenter.cn.tar.gz
-docker exec -i mysql sh -c 'exec mysql -uroot -p'${1}  < /root/mysql_backup/music_info.sql
-docker exec -i mysql sh -c 'exec mysql -uroot -p'${1}  < /root/mysql_backup/koel.sql
-docker exec -i mysql sh -c 'exec mysql -uroot -p'${1}  < /root/mysql_backup/jpress.sql
+docker exec -i mysql sh -c 'exec mysql -uroot -p'${1}  < /root/mysql/backup/music_info.sql
+docker exec -i mysql sh -c 'exec mysql -uroot -p'${1}  < /root/mysql/backup/koel.sql
+docker exec -i mysql sh -c 'exec mysql -uroot -p'${1}  < /root/mysql/backup/jpress.sql
 rm -f www.vencenter.cn.tar.gz
-rm -f /root/mysql_backup/*.sql
+rm -f /root/mysql/backup/*.sql
