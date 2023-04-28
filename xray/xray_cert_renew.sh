1bcfe780cce5d4bdc2631b4797a67df42e777f3f
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-04-28 12:29:15 +0800

    Update xray_cert_renew.sh

--- xray/xray_cert_renew.sh
+++ xray/xray_cert_renew.sh
@@ -6,16 +6,32 @@ systemctl stop nginx
 		-d "$0" \
 		--server letsencrypt \
 		--keylength ec-256 \
-		--fullchain-file /usr/local/etc/xray/cert.pem \
-		--key-file /usr/local/etc/xray/key.pem \
+		--fullchain-file /root/docker/dockerfile_work/xray/cert/cert.pem \
+		--key-file /root/docker/dockerfile_work/xray/cert/key.pem \
 		--standalone \
 		--force 
     
 echo "Xray Certificates Renewed"
 
-chmod 777 /usr/local/etc/xray/*.pem
+chmod 777 /root/docker/dockerfile_work/xray/cert/*.pem
 
 echo "Read Permission Granted for Private Key"
+
+# 更新docker项目
+bash ~/hook.sh
+# 用户名
+git config --global user.name "$1"
+# 邮箱
+git config --global user.email "$2"
+
+# 提交git
+
+# 添加到本地库
+git add /root/docker/dockerfile_work/xray/cert/*.pem
+# 提交信息
+git commit /root/docker/dockerfile_work/xray/cert/*.pem -m '🐳 chore: 证书已更新'
+# 推送
+git push
 
 systemctl start nginx
 sudo systemctl restart xray
