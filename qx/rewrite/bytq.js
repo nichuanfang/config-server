8f740504eec5b958415603da3f8eef71e824492b
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-05-09 13:07:39 +0800

    Create bytq.js

---
+++ qx/rewrite/bytq.js
@@ -0,0 +1,5 @@
+# 白云天气破解vip
+
+hostname = api.revenuecat.com
+
+^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/bytq.js
