67f6b20cba384f6297b374fdbb126c77bf5f5d3f
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-05-09 13:06:12 +0800

    Create mtxx.js

---
+++ qx/rewrite/mtxx.js
@@ -0,0 +1,5 @@
+# 破解美图秀秀
+
+hostname = *.xiuxiu.meitu.com, api.posters.meitu.com, api-sub.meitu.com
+
+^https?:\/\/((h5|api)\.xiuxiu|api-sub|api\.posters)\.meitu\.com\/.+\/(vip|user|h\d|center|home) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/mtxx.js
