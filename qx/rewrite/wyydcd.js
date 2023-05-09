56dd29eaf9ad6b62d4945064d2dd7cb901c2b642
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-05-09 13:10:47 +0800

    Create wyydcd.js

---
+++ qx/rewrite/wyydcd.js
@@ -0,0 +1,5 @@
+#网页有道词典破解vip
+
+hostname = dict.youdao.com
+
+^https:\/\/dict\.youdao\.com\/vip\/user\/status url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/wyydcd.js
