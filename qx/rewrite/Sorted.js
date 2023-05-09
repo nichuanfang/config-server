919408f66438cf2a064d661ff412ff3a0b8a15ce
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-05-09 13:09:01 +0800

    Update Sorted.js

--- qx/rewrite/Sorted.js
+++ qx/rewrite/Sorted.js
@@ -1,3 +1,5 @@
-hostname = api.sortedapp.com
+# 日历sorted破解vip
+
+ostname = api.sortedapp.com
 
 ^https:\/\/api\.sortedapp\.com\/receipts\/verify url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Sorted.js
