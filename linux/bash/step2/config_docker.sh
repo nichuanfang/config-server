d3053f5d3f02e501fed059224676801360206dff
倪传方 <1290274972@qq.com>
2023-05-18 02:34:25 +0800

    🐳 chore: 更新配置docker环境

--- linux/bash/step2/config_docker.sh
+++ linux/bash/step2/config_docker.sh
@@ -31,4 +31,15 @@ sudo systemctl enable docker
 # 查看Docker是否开启，出现绿色圆点表示服务正常开启
 sudo systemctl status docker
 
+echo "开始安装docker-compose..."
+
+# 官方git地址下载
+sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+# daocloud下载安装（速度快些）
+curl -L https://get.daocloud.io/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) >/usr/local/bin/docker-compose
+# 修改权限
+chmod +x /usr/local/bin/docker-compose
+
+echo "docker-compose安装完成!"
+
 echo "==========================================docker环境配置完成!"
