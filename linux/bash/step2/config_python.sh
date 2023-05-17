bc036d502086b3ca53d0f595880f9c80746d44bf
倪传方 <1290274972@qq.com>
2023-05-18 03:44:16 +0800

    🐳 chore: 配置pyenv

--- linux/bash/step2/config_python.sh
+++ linux/bash/step2/config_python.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
 # 配置ubuntu18的python环境
+
+echo "==========================================开始配置python环境..."
+
+# 安装pyenv
+curl https://pyenv.run | bash
+
+# 添加到环境变量
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >>~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >>~/.bashrc
+echo 'eval "$(pyenv init -)"' >>~/.bashrc
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >>/etc/profile
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >>/etc/profile
+echo 'eval "$(pyenv init -)"' >>/etc/profile
+
+# 刷新配置
+source ~/.bashrc
+source /etc/profile
+
+
+
+echo "==========================================python环境配置完成!"
