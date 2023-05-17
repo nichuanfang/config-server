0c1917275ce68cab378b498c536f58501b483bdb
倪传方 <1290274972@qq.com>
2023-05-18 05:26:27 +0800

    🐳 chore: 测试初始化服务器

--- linux/bash/step2/config_cron.sh
+++ linux/bash/step2/config_cron.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 # 配置ubuntu18的定时任务
+
+echo "==========================================开始配置定时任务..."
+
+sudo cat <<'EOF' >/etc/crontab
+# min hour day-month month day-week
+
+#每天定时清理缓存
+0 4 * * * sync && echo 3 > /proc/sys/vm/drop_caches
+
+
+EOF
+
+echo "==========================================定时任务配置完成!"
---
+++ linux/bash/step2/config_git.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 部署ubuntu18的应用
+echo "==========================================开始配置git..."
+
+mkdir -p /root/code && cd /root/code
+
+sudo apt-get install git -y
+
+
+
+echo "==========================================git配置完成!"
