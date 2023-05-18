b2e7120f355ca569bb08cf015b8a3485c28c4197
倪传方 <1290274972@qq.com>
2023-05-18 16:25:27 +0800

    fixed: release apt-get lock

--- linux/bash/step2/vps/deploy_xray.sh
+++ linux/bash/step2/vps/deploy_xray.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 # ubuntu18部署xray(vps专用)
+sudo rm -f /var/lib/dpkg/lock-frontend
+sudo rm -f /var/lib/dpkg/lock
+sudo rm -f /var/cache/apt/archives/lock
+sudo rm -f /var/lib/dpkg/lock-frontend
