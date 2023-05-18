e8f8450c08c162ca652e8dab1b2936a93db96e5a
倪传方 <1290274972@qq.com>
2023-05-18 20:37:06 +0800

    🐎 ci: 配置开机启动脚本

---
+++ linux/bash/step2/config_startup.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+echo "==========================================开始设置启动脚本..."
+
+sudo cat /dev/null >/lib/systemd/system/rc.local.service
+
+# /etc/rc.local文件的启动顺序是在网络后面，但是显然它少了[Install]段，也就没有定义如何做到开机启动，所以这样配置是无效的。因此我们就需要在后面帮他加上[Install]段：
+sudo cat <<EOF >/lib/systemd/system/rc.local.service
+#  SPDX-License-Identifier: LGPL-2.1+
+#
+#  This file is part of systemd.
+#
+#  systemd is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation; either version 2.1 of the License, or
+#  (at your option) any later version.
+
+# This unit gets pulled automatically into multi-user.target by
+# systemd-rc-local-generator if /etc/rc.local is executable.
+[Unit]
+Description=/etc/rc.local Compatibility
+Documentation=man:systemd-rc-local-generator(8)
+ConditionFileIsExecutable=/etc/rc.local
+After=network.target
+
+[Service]
+Type=forking
+ExecStart=/etc/rc.local start
+TimeoutSec=0
+RemainAfterExit=yes
+GuessMainPID=no
+root@CVM43541:/lib/systemd/system# cat /lib/systemd/system/rc.local.service 
+#  SPDX-License-Identifier: LGPL-2.1+
+#
+#  This file is part of systemd.
+#
+#  systemd is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation; either version 2.1 of the License, or
+#  (at your option) any later version.
+
+# This unit gets pulled automatically into multi-user.target by
+# systemd-rc-local-generator if /etc/rc.local is executable.
+[Unit]
+Description=/etc/rc.local Compatibility
+Documentation=man:systemd-rc-local-generator(8)
+ConditionFileIsExecutable=/etc/rc.local
+After=network.target
+
+[Service]
+Type=forking
+ExecStart=/etc/rc.local start
+TimeoutSec=0
+RemainAfterExit=yes
+GuessMainPID=no
+
+[Install]  
+WantedBy=multi-user.target 
+
+Alias=rc-local.service
+EOF
+
+# 注意 ：脚本或者开机执行的命令要在exit0之前。
+sudo cat /dev/null >/etc/rc.local
+sudo cat <<EOF >/etc/rc.local
+#!/bin/bash
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+
+# ---------------------------你的脚本---------------------------------
+cd /root/code/docker/docker-compose
+docker-compose up -d
+# ---------------------------你的脚本---------------------------------
+exit 0
+EOF
+
+systemctl daemon-reload
+systemctl enable rc-local.service
+echo "==========================================启动脚本设置成功!"
--- linux/bash/step2/deploy_app.sh
+++ linux/bash/step2/deploy_app.sh
@@ -6,13 +6,15 @@ sudo rm -f /var/lib/dpkg/lock-frontend
 sudo rm -f /var/lib/dpkg/lock
 sudo rm -f /var/cache/apt/archives/lock
 sudo rm -f /var/lib/dpkg/lock-frontend
+# 禁止nginx
+systemctl stop nginx
+systemctl disable nginx
 # 创建网络mynet
 docker network create mynet
 
 # 执行nginx初始化 $1: 域名
 cd /root/code/docker && git checkout . && git pull --allow-unrelated-histories
 /bin/bash /root/code/docker/dockerfile_work/nginx/init.sh
-
 cd /root/code/docker/docker-compose
 # 登录阿里云镜像仓库
 docker login --username=$1 --password=$2 registry.cn-hangzhou.aliyuncs.com
