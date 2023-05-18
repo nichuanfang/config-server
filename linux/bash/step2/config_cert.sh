534ba85f4f2a464c596cd028b05fd56eb586c5d8
倪传方 <1290274972@qq.com>
2023-05-18 14:25:18 +0800

    🐳 chore:  python版本控制

--- linux/bash/step2/config_cert.sh
+++ linux/bash/step2/config_cert.sh
@@ -4,7 +4,11 @@
 
 echo "==========================================开始配置证书..."
 
-
-
+cd /root/code/certbot && gp
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository universe
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install python3-certbot-nginx
 
 echo "==========================================证书配置完成!"
--- linux/bash/step2/config_python.sh
+++ linux/bash/step2/config_python.sh
@@ -32,6 +32,20 @@ pyenv install 3.8.0 -v
 # 安装3.11.0版本的python
 pyenv install 3.11.0 -v
 
+# 设置python和pip的超链接
+
+# 3.8.0
+ln -fs /root/.pyenv/shims/python3.8 /usr/bin/python3.8
+ln -fs /root/.pyenv/shims/python3.8 /usr/local/bin/python3.8
+ln -fs /root/.pyenv/shims/pip3.8 /usr/bin/pip3.8
+ln -fs /root/.pyenv/shims/pip3.8 /usr/local/bin/pip3.8
+
+# 3.11.0
+ln -fs /root/.pyenv/shims/python3.11 /usr/bin/python3.11
+ln -fs /root/.pyenv/shims/python3.11 /usr/local/bin/python3.11
+ln -fs /root/.pyenv/shims/pip3.11 /usr/bin/pip3.11
+ln -fs /root/.pyenv/shims/pip3.11 /usr/local/bin/pip3.11
+
 # 设置当前目录的python版本
 # pyenv local <python-version>
 # 取消设置当前目录的python版本
@@ -56,8 +70,8 @@ pyenv install 3.11.0 -v
 pyenv virtualenv 3.8.0 ve-3.8.0 && pyenv virtualenv 3.11.0 ve-3.11.0
 
 # 设置默认的python和pip版本为3.11.0
-echo 'pyenv activate ve-3.11.0' >>/etc/profile
-source /etc/profile
+# echo 'pyenv activate ve-3.11.0' >>/etc/profile
+# source /etc/profile
 # ----------------------------caution--------------------------------------------------
 # 推荐用法:   服务器运行python脚本/项目时 需要主动激活虚拟环境 脚本/项目运行完成后关闭虚拟环境
 # ----------------------------caution--------------------------------------------------
