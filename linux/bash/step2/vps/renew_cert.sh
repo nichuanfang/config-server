80aa19d7ab1f508dfe204f63f5afb9908370e314
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-05-19 10:46:10 +0800

    Update renew_cert.sh

--- linux/bash/step2/vps/renew_cert.sh
+++ linux/bash/step2/vps/renew_cert.sh
@@ -2,8 +2,8 @@
 
 # 更新ubuntu18的证书
 certbot renew --cert-name $1 --manual-auth-hook "/root/code/certbot/au.sh python aly add" --manual-cleanup-hook "/root/code/certbot/au.sh python aly clean"
-cp -f /etc/letsencrypt/live/$1/fullchain.pem /root/code/docker/dockerfile_work/xray/cert
-cp -f /etc/letsencrypt/live/$1/privkey.pem /root/code/docker/dockerfile_work/xray/cert
+cp -f /etc/letsencrypt/live/$1/fullchain.pem /root/code/docker/dockerfile_work/xray/cert/cert.pem
+cp -f /etc/letsencrypt/live/$1/privkey.pem /root/code/docker/dockerfile_work/xray/cert/key.pem
 echo "Certificates Renewed"
 chmod 777 /root/code/docker/dockerfile_work/xray/cert/*.pem
 cd /root/code/docker/dockerfile_work/xray/cert/
