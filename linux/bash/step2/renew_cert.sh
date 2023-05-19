480594397202a2eaa4887e7366ddb3ad93c63175
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-05-19 11:27:19 +0800

    Update renew_cert.sh

--- linux/bash/step2/renew_cert.sh
+++ linux/bash/step2/renew_cert.sh
@@ -5,8 +5,8 @@
 echo "================================================开始更新证书..."
 docker stop nginx
 certbot renew --cert-name $1 --manual-auth-hook "/root/code/certbot/au.sh python aly add" --manual-cleanup-hook "/root/code/certbot/au.sh python aly clean"
-cp /etc/letsencrypt/live/$1/fullchain.pem /opt/docker/nginx/cert/cert.pem
-cp /etc/letsencrypt/live/$1/privkey.pem /opt/docker/nginx/cert/key.pem
+cp -f /etc/letsencrypt/live/$1/fullchain.pem /opt/docker/nginx/cert/cert.pem
+cp -f /etc/letsencrypt/live/$1/privkey.pem /opt/docker/nginx/cert/key.pem
 echo "Certificates Renewed"
 chmod 777 /opt/docker/nginx/cert/*.pem
 echo "Read Permission Granted for Private Key"
