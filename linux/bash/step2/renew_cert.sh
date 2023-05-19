3ba7e9e322e6180a49ce97a409896769dbff2a6a
倪传方 <1290274972@qq.com>
2023-05-19 13:54:30 +0800

    🐎 ci: 测试vps构建工作流

--- linux/bash/step2/renew_cert.sh
+++ linux/bash/step2/renew_cert.sh
@@ -4,13 +4,14 @@
 
 echo "================================================开始更新证书..."
 docker stop nginx
+cd /root/code/docker/dockerfile_work/nginx/cert/
+git checkout . && git pull
 certbot renew --cert-name $1 --manual-auth-hook "/root/code/certbot/au.sh python aly add" --manual-cleanup-hook "/root/code/certbot/au.sh python aly clean"
 cp -f /etc/letsencrypt/live/$1/fullchain.pem /opt/docker/nginx/cert/cert.pem
 cp -f /etc/letsencrypt/live/$1/privkey.pem /opt/docker/nginx/cert/key.pem
 echo "Certificates Renewed"
 chmod 777 /opt/docker/nginx/cert/*.pem
 cp -r /opt/docker/nginx/cert/*.pem /root/code/docker/dockerfile_work/nginx/cert/
-cd /root/code/docker/dockerfile_work/nginx/cert/
 git add .
 git commit "更新站点:${1}nginx证书"
 git push
--- linux/bash/step2/vps/renew_cert.sh
+++ linux/bash/step2/vps/renew_cert.sh
@@ -2,11 +2,12 @@
 
 # 更新ubuntu18的证书
 certbot renew --cert-name $1 --manual-auth-hook "/root/code/certbot/au.sh python aly add" --manual-cleanup-hook "/root/code/certbot/au.sh python aly clean"
+cd /root/code/docker/dockerfile_work/xray/cert/
+git checkout . && git pull
 cp -f /etc/letsencrypt/live/$1/fullchain.pem /root/code/docker/dockerfile_work/xray/cert/cert.pem
 cp -f /etc/letsencrypt/live/$1/privkey.pem /root/code/docker/dockerfile_work/xray/cert/key.pem
 echo "Certificates Renewed"
 chmod 777 /root/code/docker/dockerfile_work/xray/cert/*.pem
-cd /root/code/docker/dockerfile_work/xray/cert/
 git add .
 git commit -m "证书已更新"
 git push
