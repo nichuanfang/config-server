1291a1ba380d4e1aa99132cacd2edb585428b62e
倪传方 <1290274972@qq.com>
2023-05-23 17:53:26 +0800

    ✨ feat: 每天定时启停应用服务器

--- linux/bash/step2/vps/config_cron.sh
+++ linux/bash/step2/vps/config_cron.sh
@@ -10,14 +10,10 @@ cat <<EOF >/root/cron
 30 13 1 * * /bin/bash <(curl -L https://raw.githubusercontent.com/nichuanfang/config-server/master/linux/bash/step2/vps/renew_cert.sh) $1
 #每天定时清理缓存
 30 13 1 * * sync && echo 3 >/proc/sys/vm/drop_caches
-# 每月1号数据库备份
-# 0 4 1 * * /bin/bash /root/mysql_backup/backup.sh 0820nCf9270
-# 每月1号clouddrive2更新
-# 0 4 1 * * /bin/bash <(curl -L https://raw.githubusercontent.com/nichuanfang/config-server/master/nas/clouddrive2_install.sh)
-# 每天定时更新vscode壁纸
-# 0 4 * * * /bin/bash ~/scripts/switch-vscode-bg.sh
-# 每天早上更新m3u源
-# 0 4 * * * /bin/bash <(curl -L https://ghproxy.com/https://raw.githubusercontent.com/nichuanfang/config-server/master/m3u8/update.sh)
+#每天9点启动应用服务器
+0 9 * * * curl --location -g --header "API-KEY: $2" --request PUT 'https://api.dogyun.com/cvm/server/43541/resume'
+#每天1点暂停应用服务器
+0 1 * * * curl --location -g --header "API-KEY: $2" --request PUT 'https://api.dogyun.com/cvm/server/43541/suspend'
 EOF
 
 # 载入crontab
