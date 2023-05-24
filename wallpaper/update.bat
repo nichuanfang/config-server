e99a8efa8cafd05b43ed8201d0987d0ed5cec11e
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-05-25 01:47:31 +0800

    Update update.bat

--- wallpaper/update.bat
+++ wallpaper/update.bat
@@ -1,2 +1,3 @@
+# shell:startup 加入开机启动 睡眠10s是为了等待可用网络
 timeout /t 10
 powershell curl  -o  "D:\wallpaper\everyday.jpg"  "https://ghproxy.com/https://raw.githubusercontent.com/nichuanfang/nichuanfang/main/dist/everyday.jpg";
