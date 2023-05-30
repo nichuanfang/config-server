63b780b1abfe4ad4d8471018ebeca8252ba9f2d1
nichuanfang <f18326186224@gmail.com>
2023-05-30 15:59:25 +0800

    ✨ feat: vps加入metube

---
+++ linux/bash/step2/vps/deploy_metube.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# ubuntu18部署metube(vps专用)
+sudo rm -f /var/lib/dpkg/lock-frontend
+sudo rm -f /var/lib/dpkg/lock
+sudo rm -f /var/cache/apt/archives/lock
+sudo rm -f /var/lib/dpkg/lock-frontend
+echo "=========================================开始部署metube..."
+
+# 存放临时下载文件目录
+mkdir -p /root/metube/downloads
+# 存放cookies.txt  防止有些视频无法下载
+mkdir -p /root/metube/cookies
+cd /root/code/docker/dockerfile_work/metube
+
+# 开放9999端口
+docker-compose up -d
+
+echo "=========================================metube部署完成!"
