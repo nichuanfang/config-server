8e59de1def15c1d27649890ef27cb309c33d66b6
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-07-28 02:33:34 +0800

    Update deploy_bitwarden.sh

--- linux/bash/step2/vps/deploy_bitwarden.sh
+++ linux/bash/step2/vps/deploy_bitwarden.sh
@@ -5,7 +5,7 @@ sudo rm -f /var/lib/dpkg/lock-frontend
 sudo rm -f /var/lib/dpkg/lock
 sudo rm -f /var/cache/apt/archives/lock
 sudo rm -f /var/lib/dpkg/lock-frontend
-echo "=========================================开始部署metube..."
+echo "=========================================开始部署bitwarden..."
 
 # 存放bitwarden备份文件目录
 mkdir -p /root/bitwarden/data
@@ -16,4 +16,4 @@ cp -rf /root/code/bitwarden-backup/* /root/bitwarden/data/
 # 启动bitwarden 禁止出了主用户之外的新用户注册
 docker-compose up -d
 
-echo "=========================================metube部署完成!"
+echo "=========================================bitwarden部署完成!"
