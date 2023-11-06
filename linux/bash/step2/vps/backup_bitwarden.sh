0dac7d33146dd1269f1967f1262a31a0abd5d308
nichuanfang <f18326186224@gmail.com>
2023-11-06 20:07:30 +0800

    ✨ feat: 定时u任务

--- linux/bash/step2/vps/backup_bitwarden.sh
+++ linux/bash/step2/vps/backup_bitwarden.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd /root/bitwarden/data/
+cp -rf /root/bitwarden/data/* /root/code/bitwarden-backup/
+cd /root/code/bitwarden-backup
 git add .
 git commit -m "备份bitwarden"
 git push origin main
