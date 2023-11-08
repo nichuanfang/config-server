51bf03db5fab04072efdd15caff7c49de5f66a5f
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-11-09 07:37:53 +0800

    Update deploy_nginx.sh

--- linux/bash/step2/vps/deploy_nginx.sh
+++ linux/bash/step2/vps/deploy_nginx.sh
@@ -168,6 +168,17 @@ http {
 		#     deny all;
 		# }
 
+  	       location = /api/subtitle {
+			add_header Access-Control-Allow-Origin *;
+			add_header Access-Control-Allow-Methods 'GET, POST, OPTIONS';
+			add_header Access-Control-Allow-Headers 'DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization';
+			
+			if ($request_method = 'OPTIONS') {
+        			return 204;
+    			}
+			proxy_pass https://v2.jinrishici.com/one.json;
+		}
+
 		# 博客站点
 		location / {
 			charset utf-8;
