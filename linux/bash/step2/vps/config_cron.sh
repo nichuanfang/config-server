3c9e937b46bcffb7976b060224aea593e9a4d1eb
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-11-11 00:21:20 +0800

    Update config_cron.sh

--- linux/bash/step2/vps/config_cron.sh
+++ linux/bash/step2/vps/config_cron.sh
@@ -64,7 +64,7 @@ sudo service cron restart
 touch /root/cron
 cat <<EOF >/root/cron
 # 每月1号更新证书 
-30 13 1 * * curl -s https://raw.githubusercontent.com/nichuanfang/config-server/master/linux/bash/step2/vps/renew_cert.sh | bash -s $1
+30 13 1 * * curl -s https://raw.githubusercontent.com/nichuanfang/config-server/master/linux/bash/step2/vps/renew_cert.sh | bash -s $1 $2
 # 每月1号备份bitwarden
 30 13 1 * * curl -s https://raw.githubusercontent.com/nichuanfang/config-server/master/linux/bash/step2/vps/backup_bitwarden.sh | bash
 #每天定时清理缓存
