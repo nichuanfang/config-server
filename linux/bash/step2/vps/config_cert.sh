50ce1195980a63aeab22e07643f8e51f3c7881ed
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-11-11 00:19:47 +0800

    Update config_cert.sh

--- linux/bash/step2/vps/config_cert.sh
+++ linux/bash/step2/vps/config_cert.sh
@@ -15,8 +15,8 @@ sudo apt-get update
 sudo apt-get install python3-certbot-nginx -y
 
 cd /root/code/certbot && git checkout . && git pull --allow-unrelated-histories
-
-certbot certonly -m $1@gmail.com -n --agree-tos --manual-public-ip-logging-ok -d *.$2 --manual --preferred-challenges dns --manual-auth-hook "/root/code/certbot/au.sh python aly add" --manual-cleanup-hook "/root/code/certbot/au.sh python aly clean"
+# $1: 邮箱前缀 $2: 根域名  $3: ip
+certbot certonly -m $1@gmail.com -n --agree-tos --manual-public-ip-logging-ok -d *.$2 --manual --preferred-challenges dns --manual-auth-hook "/root/code/certbot/au.sh python aly add $3" --manual-cleanup-hook "/root/code/certbot/au.sh python aly clean"
 # 验证证书
 # cat /etc/letsencrypt/live/$2/cert.pem
 # 证书的保存目录
