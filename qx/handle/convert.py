d1c5ca72765cee2e90038054076340afd52bae92
nichuanfang <f18326186224@gmail.com>
2023-11-14 13:05:22 +0800

    ✨ feat: qx配置私有化

--- qx/handle/convert.py
+++ qx/handle/convert.py
@@ -5,7 +5,7 @@ import sys
 # qx域名
 VPS_DOMAIN = sys.argv[1]
 # # qx gist id
-QX_GIST_ID = sys.argv[2]
+QX_SUBSCRIBE_ID = sys.argv[2]
 # # qx用户名
 QX_USERNAME = sys.argv[3]
 # # qx密码
@@ -17,7 +17,7 @@ with open('../qx.conf', 'r+',encoding='utf-8') as f:
 # 替换qx.conf中的qx域名
 conf = conf.replace('[VPS_DOMAIN]', f'www.{VPS_DOMAIN}')
 # # 替换qx.conf中的qx gist id
-conf = conf.replace('[QX_GIST_ID]', QX_GIST_ID)
+conf = conf.replace('[QX_SUBSCRIBE_ID]', QX_SUBSCRIBE_ID)
 # # 替换qx.conf中的qx用户名
 conf = conf.replace('[QX_USERNAME]', QX_USERNAME)
 # # 替换qx.conf中的qx密码
