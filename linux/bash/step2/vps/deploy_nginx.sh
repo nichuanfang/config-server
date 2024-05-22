7de9be7d40b4113f96d8afef98fc3eca810b0f05
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2024-05-22 23:12:03 +0800

    Update deploy_nginx.sh

--- linux/bash/step2/vps/deploy_nginx.sh
+++ linux/bash/step2/vps/deploy_nginx.sh
@@ -228,14 +228,6 @@ http {
 
   		default_type application/octet-stream;
 
-  		# metube
-		location /metube/ {
-			proxy_pass http://localhost:9999/;
-			proxy_http_version 1.1;
-			proxy_set_header Upgrade $http_upgrade;
-			proxy_set_header Connection "upgrade";
-			proxy_set_header Host $host;
-		}
 
 		# 静态站点
 		location / {
