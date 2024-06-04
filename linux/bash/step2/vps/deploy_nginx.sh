d9a647d8aa2d5f86bb455e47f98cfcff29b94b35
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2024-06-05 00:06:23 +0800

    Update deploy_nginx.sh

--- linux/bash/step2/vps/deploy_nginx.sh
+++ linux/bash/step2/vps/deploy_nginx.sh
@@ -422,6 +422,14 @@ http {
   			proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
   			proxy_set_header X-Forwarded-Proto \$scheme;
   		}
+
+    		location /webhook/watermark_remove {
+  			proxy_pass http://127.0.0.1:10004/webhook/watermark_remove;
+  			proxy_set_header Host \$host;
+  			proxy_set_header X-Real-IP \$remote_addr;
+  			proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+  			proxy_set_header X-Forwarded-Proto \$scheme;
+  		}
   	}
  
  	server {
