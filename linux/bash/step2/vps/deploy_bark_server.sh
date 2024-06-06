0927e40054c916d0332094176246e4c864cf4075
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2024-06-06 19:23:54 +0800

    Update deploy_bark_server.sh

--- linux/bash/step2/vps/deploy_bark_server.sh
+++ linux/bash/step2/vps/deploy_bark_server.sh
@@ -7,8 +7,8 @@ sudo rm -f /var/cache/apt/archives/lock
 sudo rm -f /var/lib/dpkg/lock-frontend
 echo "=========================================开始部署bark_server..."
 
-mkdir bark-server && cd bark-server
-curl -sL https://raw.githubusercontent.com/Finb/bark-server/master/deploy/docker-compose.yaml >docker-compose.yaml
+cd /root/code/docker/dockerfile_work/barkserver
+
 docker-compose up -d
 
 echo "=========================================bark_server部署完成!"
