837659bb974fbe4717e32139bee8289c321fe355
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2024-06-07 01:38:08 +0800

    Update renew_cert.sh

--- linux/bash/step2/vps/renew_cert.sh
+++ linux/bash/step2/vps/renew_cert.sh
@@ -3,6 +3,7 @@
 # 更新ubuntu18的证书  $1: 域名  $2: ip
 certbot renew --cert-name $1 --manual-auth-hook "/root/code/certbot/au.sh python aly add $2" --manual-cleanup-hook "/root/code/certbot/au.sh python aly clean"
 cd /root/code/docker/dockerfile_work/xray/cert/
+git add .
 git checkout . && git pull
 cp -f /etc/letsencrypt/live/$1/fullchain.pem /root/code/docker/dockerfile_work/xray/cert/cert.pem
 cp -f /etc/letsencrypt/live/$1/privkey.pem /root/code/docker/dockerfile_work/xray/cert/key.pem
