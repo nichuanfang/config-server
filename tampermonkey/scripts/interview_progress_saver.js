0f295c2b90c78958216714a6979d6923f0b06196
倪传方 <f18326186224@gmail.com>
2024-06-09 23:55:52 +0800

    feat[tampermonkey]: 更新保存进度脚本

--- tampermonkey/scripts/interview_progress_saver.js
+++ tampermonkey/scripts/interview_progress_saver.js
@@ -4,7 +4,7 @@
 // @version      1.0
 // @description  Save and restore progress on the interview website
 // @author       ncf
-// @match        https://doc-blond-eight.vercel.app/interview*
+// @match        https://doc.jaychou.site/interview/*
 // @grant        GM_setValue
 // @grant        GM_getValue
 // ==/UserScript==
@@ -12,19 +12,19 @@
 (function() {
     'use strict';
 
-    var firstURL = window.location.href;
+    let firstURL = window.location.href;
     // Function to save the current URL to localStorage every few seconds
     function saveProgress() {
-        var currentURL = window.location.href;
-        if (currentURL.startsWith('https://doc-blond-eight.vercel.app/interview/')) {
+        let currentURL = window.location.href;
+        if (currentURL.startsWith('https://doc.jaychou.site/interview/')) {
             localStorage.setItem('interview_progress', currentURL);
         }
     }
 
     // Function to check for saved progress and restore it
     function restoreProgress() {
-        var savedProgress = localStorage.getItem('interview_progress');
-        if (savedProgress && firstURL === 'https://doc-blond-eight.vercel.app/interview') {
+        let savedProgress = localStorage.getItem('interview_progress');
+        if (savedProgress && firstURL === 'https://doc.jaychou.site/interview/') {
             window.location.replace(savedProgress);
         }
     }
