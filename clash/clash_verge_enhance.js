072e847986e59b3e98dc885a38c761123d18d867
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2024-07-29 01:00:51 +0800

    Update clash_verge_enhance.js

--- clash/clash_verge_enhance.js
+++ clash/clash_verge_enhance.js
@@ -20,11 +20,11 @@ function overwriteRules(params) {
         "GEOIP,LAN,DIRECT,no-resolve",
         "GEOIP,CN,DIRECT,no-resolve",
         "RULE-SET,applications,DIRECT",
-        "DOMAIN-SUFFIX,chatgpt.com,US-自动选择",
         "RULE-SET,tld-not-cn," + proxyName,
         "RULE-SET,google," + proxyName,
         "RULE-SET,icloud," + proxyName,
         "RULE-SET,apple," + proxyName,
+        "DOMAIN-SUFFIX,chatgpt.com,US-自动选择",
         "RULE-SET,gfw," + proxyName,
         "RULE-SET,greatfire," + proxyName,
         "RULE-SET,telegramcidr," + proxyName,
@@ -35,98 +35,98 @@ function overwriteRules(params) {
         reject: {
             type: "http",
             behavior: "domain",
-            url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/reject.txt",
+            url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/reject.txt",
             path: "./ruleset/reject.yaml",
             interval: 86400,
         },
         icloud: {
             type: "http",
             behavior: "domain",
-            url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/icloud.txt",
+            url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/icloud.txt",
             path: "./ruleset/icloud.yaml",
             interval: 86400,
         },
         apple: {
             type: "http",
             behavior: "domain",
-            url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/apple.txt",
+            url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/apple.txt",
             path: "./ruleset/apple.yaml",
             interval: 86400,
         },
         google: {
             type: "http",
             behavior: "domain",
-            url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/google.txt",
+            url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/google.txt",
             path: "./ruleset/google.yaml",
             interval: 86400,
         },
         proxy: {
             type: "http",
             behavior: "domain",
-            url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/proxy.txt",
+            url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/proxy.txt",
             path: "./ruleset/proxy.yaml",
             interval: 86400,
         },
         direct: {
             type: "http",
             behavior: "domain",
-            url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/direct.txt",
+            url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/direct.txt",
             path: "./ruleset/direct.yaml",
             interval: 86400,
         },
         private: {
             type: "http",
             behavior: "domain",
-            url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/private.txt",
+            url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/private.txt",
             path: "./ruleset/private.yaml",
             interval: 86400,
         },
         gfw: {
             type: "http",
             behavior: "domain",
-            url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/gfw.txt",
+            url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/gfw.txt",
             path: "./ruleset/gfw.yaml",
             interval: 86400,
         },
         greatfire: {
             type: "http",
             behavior: "domain",
-            url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/greatfire.txt",
+            url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/greatfire.txt",
             path: "./ruleset/greatfire.yaml",
             interval: 86400,
         },
         "tld-not-cn": {
             type: "http",
             behavior: "domain",
-            url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/tld-not-cn.txt",
+            url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/tld-not-cn.txt",
             path: "./ruleset/tld-not-cn.yaml",
             interval: 86400,
         },
         telegramcidr: {
             type: "http",
             behavior: "ipcidr",
-            url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/telegramcidr.txt",
+            url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/telegramcidr.txt",
             path: "./ruleset/telegramcidr.yaml",
             interval: 86400,
         },
         cncidr: {
             type: "http",
             behavior: "ipcidr",
-            url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/cncidr.txt",
+            url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/cncidr.txt",
             path: "./ruleset/cncidr.yaml",
             interval: 86400,
         },
         lancidr: {
             type: "http",
             behavior: "ipcidr",
-            url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/lancidr.txt",
+            url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/lancidr.txt",
             path: "./ruleset/lancidr.yaml",
             interval: 86400,
         },
         applications: {
             type: "http",
             behavior: "classical",
-            url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/applications.txt",
+            url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/applications.txt",
             path: "./ruleset/applications.yaml",
             interval: 86400,
         },
@@ -234,53 +234,62 @@ function overwriteProxyGroups(params) {
 }
 //防止dns泄露
 function overwriteDns(params) {
-  const cnDnsList = [
-    'https://1.12.12.12/dns-query',
-    // 'https://223.5.5.5/dns-query',
-    '180.184.1.1'
-  ]
-  
-  // 大部分的网络请求都会走这个里面的，这里目前是腾讯、阿里、和使用节点查询的1.0.0.1的dns
-  const trustDnsList = [
-    // 'https://doh.pub/dns-query', // 腾讯
-    // 'https://dns.alidns.com/dns-query', // 阿里（这里会触发h3和普通的并发查询）
-    // '180.184.1.1', // 字节-火山引擎的DNS
-    "https://1.0.0.1/dns-query",
-    "https://1.1.1.1/dns-query",
-  ]
-   const notionDns = 'tls://dns.jerryw.cn' // notion加速dns
-   const notionUrls = [
-     'http-inputs-notion.splunkcloud.com',
-     '+.notion-static.com',
-     '+.notion.com',
-     '+.notion.new',
-     '+.notion.site',
-     '+.notion.so',
-   ]
-   const combinedUrls = notionUrls.join(',');
-   const dnsOptions = {
-    'enable': true,
-    'prefer-h3': true, // 如果DNS服务器支持DoH3会优先使用h3（本例子中只有阿里DNS支持）
-    'default-nameserver': cnDnsList, // 用于解析其他DNS服务器、和节点的域名, 必须为IP, 可为加密DNS。注意这个只用来解析节点和其他的dns，其他网络请求不归他管
-    'nameserver': trustDnsList, // 其他网络请求都归他管
-    
-    // 这个用于覆盖上面的 nameserver
-    'nameserver-policy': {
-      [combinedUrls]: notionDns,
-      'geosite:geolocation-!cn': trustDnsList,
-      // 如果你有一些内网使用的DNS，应该定义在这里，多个域名用英文逗号分割
-      // '+.公司域名.com, www.4399.com, +.baidu.com': '10.0.0.1'
-     },
-    }
+    const cnDnsList = [
+        "https://1.12.12.12/dns-query",
+        "https://223.5.5.5/dns-query",
+    ];
+    const trustDnsList = [
+        // 'quic://dns.cooluc.com',
+        "https://1.0.0.1/dns-query",
+        "https://1.1.1.1/dns-query",
+    ];
+    // const notionDns = 'tls://dns.jerryw.cn'
+    // const notionUrls = [
+    //     'http-inputs-notion.splunkcloud.com',
+    //     '+.notion-static.com',
+    //     '+.notion.com',
+    //     '+.notion.new',
+    //     '+.notion.site',
+    //     '+.notion.so',
+    // ]
+    // const combinedUrls = notionUrls.join(',');
+    const dnsOptions = {
+        enable: true,
+        "prefer-h3": true, // 如果DNS服务器支持DoH3会优先使用h3
+        "default-nameserver": cnDnsList, // 用于解析其他DNS服务器、和节点的域名, 必须为IP, 可为加密DNS。注意这个只用来解析节点和其他的dns，其他网络请求不归他管
+        nameserver: trustDnsList, // 其他网络请求都归他管
+
+        // 这个用于覆盖上面的 nameserver
+        "nameserver-policy": {
+            //[combinedUrls]: notionDns,
+            "geosite:cn": cnDnsList,
+            "geosite:geolocation-!cn": trustDnsList,
+            // 如果你有一些内网使用的DNS，应该定义在这里，多个域名用英文逗号分割
+            // '+.公司域名.com, www.4399.com, +.baidu.com': '10.0.0.1'
+        },
+        fallback: trustDnsList,
+        "fallback-filter": {
+            geoip: true,
+            //除了 geoip-code 配置的国家 IP, 其他的 IP 结果会被视为污染 geoip-code 配置的国家的结果会直接采用，否则将采用 fallback结果
+            "geoip-code": "CN",
+            //geosite 列表的内容被视为已污染，匹配到 geosite 的域名，将只使用 fallback解析，不去使用 nameserver
+            geosite: ["gfw"],
+            ipcidr: ["240.0.0.0/4"],
+            domain: ["+.google.com", "+.facebook.com", "+.youtube.com"],
+        },
+    };
+
     // GitHub加速前缀
     const githubPrefix = "https://fastgh.lainbo.com/";
 
-     // GEO数据GitHub资源原始下载地址
+    // GEO数据GitHub资源原始下载地址
     const rawGeoxURLs = {
-        geoip: 'https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.dat',
-        geosite: 'https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat',
-        mmdb: 'https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/country-lite.mmdb',
-    }
+        geoip:
+            "https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.dat",
+        geosite:
+            "https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat",
+        mmdb: "https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/country-lite.mmdb",
+    };
 
     // 生成带有加速前缀的GEO数据资源对象
     const accelURLs = Object.fromEntries(
@@ -311,10 +320,6 @@ function overwriteDns(params) {
         },
         "geodata-mode": true,
         "geox-url": accelURLs,
-        'geo-auto-update': true,
-        'geo-update-interval': 24,
-        'geodata-loader': 'standard',
-        'find-process-mode': 'strict',
     };
 
     params.dns = { ...params.dns, ...dnsOptions };
