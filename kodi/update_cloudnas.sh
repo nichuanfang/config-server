5f24e18624ef2757614dce951074bea1a2e32ab0
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2024-08-17 13:13:59 +0800

    Update update_cloudnas.sh

--- kodi/update_cloudnas.sh
+++ kodi/update_cloudnas.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #github代理
-GITHUB_PROXY=$1
+GITHUB_PROXY=${1:-'proxy.jaychou.site'}
 # dockerhub代理
-DOCKER_HUB_PROXY=$2
+DOCKER_HUB_PROXY=${2:-'docker-proxy.jaychou.site'}
 #检测是否需要更新
 
 latest_version=$(curl -s "https://$GITHUB_PROXY/https://hub.docker.com/v2/repositories/cloudnas/clouddrive2/tags/" | python3 -c "import sys, json; print(json.load(sys.stdin)['results'][1]['name'])")
@@ -15,8 +15,8 @@ current_version=$(docker images | grep cloudnas/clouddrive2 | awk '{print $2}')
 echo "当前版本: $current_version"
 
 if [ -n "$current_version" ] && [ "$latest_version" == "$current_version" ]; then
-echo  "无需更新cloudnas"
-exit 0
+      echo "无需更新cloudnas"
+      exit 0
 fi
 
 # clouddrive2 install script for CoreELEC
@@ -27,20 +27,20 @@ mount --make-shared /storage
 
 #创建相应目录赋予权限     [   ]  内  前后都要有空格 if与[ ]之间也要有空格
 if [ ! -d "/storage/cloudnas" ]; then
-mkdir -p /storage/cloudnas
-chmod 777 /storage/cloudnas
+      mkdir -p /storage/cloudnas
+      chmod 777 /storage/cloudnas
 fi
 
 if [ ! -d "/storage/clouddrive2/config" ]; then
-mkdir -p /storage/clouddrive2/config
-chmod 777 /storage/clouddrive2/config
+      mkdir -p /storage/clouddrive2/config
+      chmod 777 /storage/clouddrive2/config
 fi
 
 #更新clouddrive2
 echo "开始更新clouddrive2镜像..."
 if [ -n "$current_version" ]; then
-docker rm -f clouddrive2
-docker rmi -f cloudnas/clouddrive2:"$current_version"
+      docker rm -f clouddrive2
+      docker rmi -f cloudnas/clouddrive2:"$current_version"
 fi
 docker pull $DOCKER_HUB_PROXY/cloudnas/clouddrive2:"$latest_version"
 docker tag $DOCKER_HUB_PROXY/cloudnas/clouddrive2:"$latest_version" cloudnas/clouddrive2:"$latest_version"
@@ -48,10 +48,10 @@ docker rmi -f $DOCKER_HUB_PROXY/cloudnas/clouddrive2:"$latest_version"
 echo "clouddrive2镜像更新完毕!"
 
 # 删除原挂载目录
-if [ -d  "/storage/cloudnas/CloudDrive" ]; then
-#  卸载挂载目录
-umount /storage/cloudnas/CloudDrive
-rm -rf /storage/cloudnas/CloudDrive
+if [ -d "/storage/cloudnas/CloudDrive" ]; then
+      #  卸载挂载目录
+      umount /storage/cloudnas/CloudDrive
+      rm -rf /storage/cloudnas/CloudDrive
 fi
 
 echo "Clouddrive2更新完毕!"
@@ -67,9 +67,9 @@ docker run -d \
       -v /storage/clouddrive2/config:/Config \
       --network host \
       --pid host \
-     --privileged \
-     --device /dev/fuse:/dev/fuse \
-     cloudnas/clouddrive2:"$latest_version"
+      --privileged \
+      --device /dev/fuse:/dev/fuse \
+      cloudnas/clouddrive2:"$latest_version"
 
 echo "clouddrive2启动完成!"
 
