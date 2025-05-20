e5843e170ab1d580c4533ac04f8e0431609b46cc
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2025-05-20 22:32:02 +0800

    Update deploy_metube.sh

--- linux/bash/step2/vps/deploy_metube.sh
+++ linux/bash/step2/vps/deploy_metube.sh
@@ -8,10 +8,10 @@ sudo rm -f /var/lib/dpkg/lock-frontend
 echo "=========================================开始部署metube..."
 
 # 存放临时下载文件目录
-mkdir -p /root/metube/downloads
+mkdir -p ~/data/metube/downloads
 # 存放cookies.txt  防止有些视频无法下载
-mkdir -p /root/metube/cookies
-cd /root/code/docker/dockerfile_work/metube
+mkdir -p ~/data/metube/cookies
+cd ~/code/docker/dockerfile_work/metube
 
 # 开放9999端口
 docker-compose up -d
