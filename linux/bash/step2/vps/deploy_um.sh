4006d97a2406310c04df16404ba78605d37a6755
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2025-05-20 23:12:54 +0800

    Create deploy_um.sh

---
+++ linux/bash/step2/vps/deploy_um.sh
@@ -0,0 +1,19 @@
+# 音乐解锁
+
+# 源代码仓库: https://git.unlock-music.dev/um/web
+
+# 通过自行git拉取最新代码构建更新 docker/dockerfile_work/nginx/web/unblock-music的dist文件
+
+# 构建说明
+
+## 环境要求: nodejs (v22.x) + npm
+
+## 安装依赖
+```bash
+npm install
+npm ci
+```
+
+## 构建  构建后的产物可以在 dist 目录找到。
+npm run build
+
