1fa1586a2f6d5b62ab6ab150363a2f11c059dfa8
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2025-05-20 22:28:57 +0800

    Update deploy_bitwarden.sh

--- linux/bash/step2/vps/deploy_bitwarden.sh
+++ linux/bash/step2/vps/deploy_bitwarden.sh
@@ -8,11 +8,11 @@ sudo rm -f /var/lib/dpkg/lock-frontend
 echo "=========================================开始部署bitwarden..."
 
 # 存放bitwarden备份文件目录
-mkdir -p /root/bitwarden/data
+mkdir -p ~/data/bitwarden
 cd /root/code/docker/dockerfile_work/bitwarden
 
 # 恢复备份数据
-cp -rf /root/code/bitwarden-backup/* /root/bitwarden/data/
+cp -rf /root/code/bitwarden-backup/* ~/data/bitwarden
 # 启动bitwarden 禁止出了主用户之外的新用户注册
 docker-compose up -d
 
