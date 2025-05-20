5b6f31ac918f328dffb90122fd171b455c812f82
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2025-05-20 12:57:31 +0800

    Create bashrc.sh

---
+++ linux/bash/step1/bashrc.sh
@@ -0,0 +1,11 @@
+#自定义别名
+alias ...='cd ../../'
+alias ..='cd ../'
+alias .='ls'
+alias ~='cd ~'
+alias ud='docker-compose down && docker-compose pull && docker-compose up -d && docker image prune -af'
+alias gp='git pull --rebase --autostash'
+
+cd() {
+    builtin cd "$@" && ls
+}
