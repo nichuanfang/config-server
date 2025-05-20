748645ffd82a48e13f80d02e140509cc172b7da4
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2025-05-20 22:55:39 +0800

    Create deploy_frps.sh

---
+++ linux/bash/step2/vps/deploy_frps.sh
@@ -0,0 +1,25 @@
+# 部署frps
+
+# 创建frps文件夹
+mkdir -p ~/data/frp
+
+# 下载frps和配置文件(自行准备)
+
+# 创建 frps.service 文件
+cat <<EOF > /etc/systemd/system/frps.service
+[Unit]
+Description = frp server
+After = network.target syslog.target
+Wants = network.target
+[Service]
+Type = simple
+ExecStart = ~/data/frp/frps -c ~/data/frp/frps.toml
+[Install]
+WantedBy = multi-user.target
+EOF
+
+# 开机启动
+sudo systemctl enable frps
+
+# 启动frp
+sudo systemctl start frps
