ffacc189a07555c2b8259c6ebaa33af84b580217
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2025-05-20 21:33:42 +0800

    Update deploy_memos.sh

--- linux/bash/step2/vps/deploy_memos.sh
+++ linux/bash/step2/vps/deploy_memos.sh
@@ -7,6 +7,12 @@ sudo rm -f /var/cache/apt/archives/lock
 sudo rm -f /var/lib/dpkg/lock-frontend
 echo "=========================================开始部署memos..."
 
+# 存放memos备份文件目录
+mkdir -p ~/data/memos
+
+# 恢复备份数据 
+
+#部署
 cd /root/code/docker/dockerfile_work/memos || exit
 
 docker-compose up -d
